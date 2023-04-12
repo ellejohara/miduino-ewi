c87e35895fd1ef8f875757ba01200aa848af0054
ellejohara <ellejohara@gmail.com>
2023-04-12 14:47:45 -0700

    Create 5603-MIDuino_Send.ino

---
+++ 5603-MIDuino_Send.ino
@@ -0,0 +1,39 @@
+#define NOTE_ON  0x0090 // 144
+#define NOTE_OFF 0x0080 // 128
+#define VELOCITY 0x007F // 127
+
+int thePins[] = {2};
+#define NUM_PINS sizeof(thePins);
+
+int note = 60;
+
+bool buttonState, prevState;
+
+void setup() {
+  for (int i = 0; i < NUM_PINS; i++) {
+    pinMode(thePins[i], INPUT_PULLUP);
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  buttonState = !digitalRead(2);
+  
+  if (buttonState != prevState) {
+    if (buttonState == HIGH) {
+      sendMidi(144, 60, 127);
+    }
+    
+    if (buttonState == LOW) {
+      sendMidi(128, 60, 127);
+    }
+  }
+  
+  prevState = buttonState;
+}
+
+void sendMidi(byte command, byte data1, byte data2) {
+  Serial.write(command);
+  Serial.write(data1);
+  Serial.write(data2);
+}
